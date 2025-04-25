c098bc885d5f36a8bf51dda4402068987157bc62
retlim <se.wi.public@gmail.com>
2025-04-25 16:21:39 +0200

    Add unit tests for external metadata builder

---
+++ tests/Metadata/External/Builder/BuilderTest.php
@@ -0,0 +1,165 @@
+<?php
+/**
+ * Fusion. A package manager for PHP-based projects.
+ * Copyright Valvoid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Valvoid\Fusion\Tests\Metadata\External\Builder;
+
+use Throwable;
+use Valvoid\Fusion\Metadata\External\Builder;
+use Valvoid\Fusion\Tests\Metadata\External\Builder\Mocks\ContainerMock;
+use Valvoid\Fusion\Tests\Test;
+
+/**
+ *
+ * @copyright Valvoid
+ * @license GNU GPLv3
+ */
+class BuilderTest extends Test
+{
+    /** @var string|array  */
+    protected string|array $coverage = Builder::class;
+
+    /** @var ContainerMock  */
+    protected ContainerMock $container;
+
+    protected Builder $builder;
+
+    public function __construct()
+    {
+        $this->container = new ContainerMock;
+        $this->builder = new Builder("/dir", "api/path/1.0.0");
+
+        $this->testId();
+        $this->testRawDir();
+
+        // pattern reference
+        $this->testSource();
+
+        // absolute/pointer top reference
+        $this->testNormalizeReference();
+        $this->testProductionLayer();
+        $this->testMetadata();
+
+        $this->container->destroy();
+    }
+
+    public function testProductionLayer(): void
+    {
+        try {
+            $metadata = [
+                "id" => "path",
+                "version" => "2.3.4",
+                "environment" => ["environment"],
+                "structure" => [
+                    "/cache" => "cache"
+                ]
+            ];
+
+            $this->builder->addProductionLayer(
+                json_encode($metadata),
+                "fusion.json"
+            );
+
+            return;
+
+        } catch (Throwable $e) {
+            $this->handleFailedTest();
+        }
+    }
+
+    public function testMetadata(): void
+    {
+        if ($this->builder->getMetadata()->getContent() !== [
+                "id" => "path",
+                "version" => "2.3.4",
+                "environment" => [
+                    "environment",
+                    "php" => [
+                        "modules" => []
+                    ]
+                ],
+                "structure" => [
+                    "cache" => "/cache",
+                    "sources" => [],
+                    "extensions" => [],
+                    "namespaces" => [],
+                    "states" => []
+                ],
+                "source" => [
+                    "api" => "api",
+                    "path" => "/path",
+                    "prefix" => "",
+                    "reference" => "2.3.4"
+                ],
+                "dir" => "/dir/path",
+                "dependencies" => [
+                    "production" => []
+                ]
+            ])
+            $this->handleFailedTest();
+    }
+
+    public function testNormalizeReference(): void
+    {
+        $source = [
+            "api" => "api",
+            "path" => "/path",
+            "prefix" => "",
+            "reference" => "2.3.4"
+        ];
+
+        $this->builder->normalizeReference("2.3.4");
+
+        if ($this->builder->getNormalizedSource() !== $source ||
+            $this->builder->getParsedSource() !== $source)
+            $this->handleFailedTest();
+    }
+
+    public function testSource(): void
+    {
+        $source = [
+            "api" => "api",
+            "path" => "/path",
+            "prefix" => "",
+            "reference" => [[
+                "build" => "",
+                "release" => "",
+                "major" => "1",
+                "minor" => "0",
+                "patch" => "0",
+                "sign" => ""
+            ]]
+        ];
+
+        if ($this->builder->getNormalizedSource() !== $source ||
+            $this->builder->getParsedSource() !== $source)
+            $this->handleFailedTest();
+    }
+
+    public function testId(): void
+    {
+        if ($this->builder->getId() !== "path")
+            $this->handleFailedTest();
+    }
+
+    public function testRawDir(): void
+    {
+        if ($this->builder->getRawDir() !== "/dir")
+            $this->handleFailedTest();
+    }
+}
---
+++ tests/Metadata/External/Builder/Mocks/ContainerMock.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Fusion. A package manager for PHP-based projects.
+ * Copyright Valvoid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Valvoid\Fusion\Tests\Metadata\External\Builder\Mocks;
+
+use Closure;
+use ReflectionClass;
+use Valvoid\Fusion\Bus\Events\Event;
+use Valvoid\Fusion\Container\Container;
+use Valvoid\Fusion\Container\Proxy\Proxy;
+use Valvoid\Fusion\Bus\Proxy\Proxy as BusProxy;
+
+/**
+ * Mocked container.
+ *
+ * @copyright Valvoid
+ * @license GNU GPLv3
+ */
+class ContainerMock
+{
+    private ReflectionClass $reflection;
+
+    public $proxy;
+
+    public function __construct()
+    {
+        $this->proxy = new class implements Proxy {
+
+            public $bus;
+
+            public function __construct()
+            {
+                $this->bus = new class implements BusProxy
+                {
+                    public function addReceiver(string $id, Closure $callback, string ...$events): void {}
+                    public function broadcast(Event $event): void{}
+                    public function removeReceiver(string $id, string ...$events): void {}
+                };
+            }
+
+            public function get(string $class, ...$args): object
+            {
+                return match($class) {
+                    "Valvoid\Fusion\Bus\Proxy\Proxy" => $this->bus
+                };
+            }
+
+            public function refer(string $id, string $class): void {}
+            public function unset(string $class): void {}
+        };
+        $this->reflection = new ReflectionClass(Container::class);
+        $this->reflection->setStaticPropertyValue("instance", new class($this->proxy) extends Container
+        {
+            public function __construct(protected Proxy $proxy){}
+        });
+    }
+
+    public function destroy(): void
+    {
+        $this->reflection->setStaticPropertyValue("instance", null);
+    }
+}
