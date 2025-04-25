b357731217cb56b2c192c5a81ef783b227efab40
retlim <se.wi.public@gmail.com>
2025-04-25 20:26:30 +0200

    Instantiate unit tests automatically

--- tests/units.php
+++ tests/units.php
@@ -31,120 +31,31 @@ spl_autoload_register(function (string $loadable) use ($root, $lazy)
     require $root . $lazy[$loadable];
 });
 
-/** @var Test[] $tests */
-$tests = [
-    new Valvoid\Fusion\Tests\Bus\BusTest,
-    new Valvoid\Fusion\Tests\Container\ContainerTest,
-    new Valvoid\Fusion\Tests\Config\ConfigTest,
-    new Valvoid\Fusion\Tests\Config\Parser\DirTest,
-    new Valvoid\Fusion\Tests\Config\Parser\Hub\HubTest,
-    new Valvoid\Fusion\Tests\Config\Parser\Log\LogTest,
-    new Valvoid\Fusion\Tests\Config\Parser\ParserTest,
-    new Valvoid\Fusion\Tests\Config\Parser\Tasks\TasksTest,
-    new Valvoid\Fusion\Tests\Config\Interpreter\InterpreterTest,
-    new Valvoid\Fusion\Tests\Config\Interpreter\DirTest,
-    new Valvoid\Fusion\Tests\Config\Interpreter\PersistenceTest,
-    new Valvoid\Fusion\Tests\Config\Interpreter\HubTest,
-    new Valvoid\Fusion\Tests\Config\Interpreter\LogTest,
-    new Valvoid\Fusion\Tests\Config\Interpreter\TasksTest,
-    new Valvoid\Fusion\Tests\Dir\DirTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\Bitbucket\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\Bitbucket\Config\NormalizerTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\Bitbucket\Config\ParserTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\Bitbucket\BitbucketTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\GitHub\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\GitHub\Config\NormalizerTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\GitHub\Config\ParserTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\GitHub\GitHubTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\GitLab\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\GitLab\Config\NormalizerTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\GitLab\Config\ParserTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\GitLab\GitLabTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\Valvoid\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\Valvoid\Config\NormalizerTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\Valvoid\Config\ParserTest,
-    new Valvoid\Fusion\Tests\Hub\APIs\Remote\Valvoid\ValvoidTest,
-    new Valvoid\Fusion\Tests\Hub\Cache\CacheTest,
-    new Valvoid\Fusion\Tests\Hub\HubTest,
-    new Valvoid\Fusion\Tests\Metadata\Interpreter\EnvironmentTest,
-    new Valvoid\Fusion\Tests\Metadata\Interpreter\InterpreterTest,
-    new Valvoid\Fusion\Tests\Metadata\Interpreter\LifecycleTest,
-    new Valvoid\Fusion\Tests\Metadata\Interpreter\StructureTest,
-    new Valvoid\Fusion\Tests\Metadata\Normalizer\CacheTest,
-    new Valvoid\Fusion\Tests\Metadata\Normalizer\ExtensionTest,
-    new Valvoid\Fusion\Tests\Metadata\Normalizer\NormalizerTest,
-    new Valvoid\Fusion\Tests\Metadata\Normalizer\LoadableTest,
-    new Valvoid\Fusion\Tests\Metadata\Normalizer\SourceTest,
-    new Valvoid\Fusion\Tests\Metadata\Normalizer\StateTest,
-    new Valvoid\Fusion\Tests\Metadata\Normalizer\StructureTest,
-    new Valvoid\Fusion\Tests\Metadata\Parser\ParserTest,
-    new Valvoid\Fusion\Tests\Metadata\Parser\EnvironmentTest,
-    new Valvoid\Fusion\Tests\Metadata\Parser\StructureTest,
-    new Valvoid\Fusion\Tests\Metadata\Internal\InternalTest,
-    new Valvoid\Fusion\Tests\Metadata\External\ExternalTest,
-    new Valvoid\Fusion\Tests\Metadata\External\Normalizer\ReferenceTest,
-    new Valvoid\Fusion\Tests\Metadata\External\Parser\SourceTest,
-    new Valvoid\Fusion\Tests\Metadata\External\Builder\BuilderTest,
-    new Valvoid\Fusion\Tests\Metadata\Internal\Builder\BuilderTest,
-    new Valvoid\Fusion\Tests\Log\LogTest,
-    new Valvoid\Fusion\Tests\Util\Version\InterpreterTest,
-    new Valvoid\Fusion\Tests\Util\Reference\NormalizerTest,
-    new Valvoid\Fusion\Tests\Util\Metadata\StructureTest,
-    new Valvoid\Fusion\Tests\Util\Pattern\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Build\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Build\Config\NormalizerTest,
-    new Valvoid\Fusion\Tests\Tasks\Build\Config\ParserTest,
-    new Valvoid\Fusion\Tests\Tasks\Build\SAT\GraphTest,
-    new Valvoid\Fusion\Tests\Tasks\Build\SAT\ClauseTest,
-    new Valvoid\Fusion\Tests\Tasks\Build\SAT\SolverTest,
-    new Valvoid\Fusion\Tests\Tasks\Build\BuildTest,
-    new Valvoid\Fusion\Tests\Tasks\Copy\CopyTest,
-    new Valvoid\Fusion\Tests\Tasks\Copy\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Categorize\CategorizeTest,
-    new Valvoid\Fusion\Tests\Tasks\Categorize\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Download\DownloadTest,
-    new Valvoid\Fusion\Tests\Tasks\Download\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Extend\ExtendTest,
-    new Valvoid\Fusion\Tests\Tasks\Extend\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Image\ImageTest,
-    new Valvoid\Fusion\Tests\Tasks\Image\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Inflate\InflateTest,
-    new Valvoid\Fusion\Tests\Tasks\Inflate\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Register\RegisterTest,
-    new Valvoid\Fusion\Tests\Tasks\Register\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Replicate\ReplicateTest,
-    new Valvoid\Fusion\Tests\Tasks\Replicate\Config\ParserTest,
-    new Valvoid\Fusion\Tests\Tasks\Replicate\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Replicate\Config\NormalizerTest,
-    new Valvoid\Fusion\Tests\Tasks\Shift\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Shift\ShiftTest,
-    new Valvoid\Fusion\Tests\Tasks\Snap\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Snap\SnapTest,
-    new Valvoid\Fusion\Tests\Tasks\Stack\Config\InterpreterTest,
-    new Valvoid\Fusion\Tests\Tasks\Stack\StackTest,
-    new Valvoid\Fusion\Tests\Group\GroupTest
-];
-
-foreach ($tests as $test) {
-    if (!$test->getResult())
-        $result = 1;
-
-    $coverage = $test->getCoverage();
-
-    // array, string, null
-    if (is_array($coverage)) {
-        foreach ($coverage as $coverageItem)
-            if (!in_array($coverageItem, $coverageClassnames) &&
-                in_array($coverageItem, $classnames))
-                $coverageClassnames[] = $coverageItem;
-
-    } elseif (is_string($coverage) &&
-        !in_array($coverage, $coverageClassnames) &&
-        in_array($coverage, $classnames))
-        $coverageClassnames[] = $coverage;
-}
-
 try {
+    foreach ($lazy as $classname => $dir) {
+        $reflection = new ReflectionClass($classname);
+
+        if ($reflection->isSubclassOf(Test::class)) {
+            $test = $reflection->newInstance();
+
+            if (!$test->getResult())
+                $result = 1;
+
+            $coverage = $test->getCoverage();
+
+            // array, string, null
+            if (is_array($coverage)) {
+                foreach ($coverage as $coverageItem)
+                    if (!in_array($coverageItem, $coverageClassnames) &&
+                        in_array($coverageItem, $classnames))
+                        $coverageClassnames[] = $coverageItem;
+
+            } elseif (is_string($coverage) &&
+                !in_array($coverage, $coverageClassnames) &&
+                in_array($coverage, $classnames))
+                $coverageClassnames[] = $coverage;
+        }
+    }
 
     // simple coverage by classname
     foreach ($classnames as $i => $classname) {
