6065e5b78fb75727a88a742ea67d883f4ef15c97
retlim <se.wi.public@gmail.com>
2025-05-13 15:16:16 +0200

    Add unit tests for archive cache request

---
+++ tests/Hub/Requests/Cache/Archive/Mocks/APIMock.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Fusion. A package manager for PHP-based projects.
+ * Copyright Valvoid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace Valvoid\Fusion\Tests\Hub\Requests\Cache\Archive\Mocks;
+
+use Valvoid\Fusion\Hub\APIs\Local\Local;
+use Valvoid\Fusion\Hub\Responses\Local\Archive;
+use Valvoid\Fusion\Hub\Responses\Local\File;
+use Valvoid\Fusion\Hub\Responses\Local\References;
+
+/**
+ * @copyright Valvoid
+ * @license GNU GPLv3
+ */
+class APIMock extends Local
+{
+    public function __construct(){}
+    public function getFileContent(string $path, string $reference, string $filename): File|string {return "";}
+    public function getReferences(string $path): References|string {return "";}
+    public function createArchive(string $path, string $reference, string $dir): Archive|string {return "";}
+}
